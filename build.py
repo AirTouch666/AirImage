8adbd449070bdcf56c22dad6a94bec09a8d70a47
AirTouch666 <airtouch97@gmail.com>
2024-10-22 21:19:51 +0800

    📝Chore:Build app

---
+++ build.py
@@ -0,0 +1,15 @@
+import PyInstaller.__main__
+import os
+
+# 获取当前脚本的目录
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+PyInstaller.__main__.run([
+    'run.py',
+    '--name=AirImage',
+    '--onefile',
+    '--add-data=templates:templates',
+    '--add-data=static:static',
+    '--icon=static/favicon.ico',
+    '--hidden-import=PIL._tkinter_finder'
+])
